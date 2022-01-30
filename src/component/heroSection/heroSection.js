a45e3fee78f74b80155295dc24cdc4567cf94671
Culeayush <ajdkma52813@gmail.com>
2022-01-30 22:25:27 +0530

    MODIFIED HEROSECTION

--- src/component/heroSection/heroSection.js
+++ src/component/heroSection/heroSection.js
@@ -1,74 +1,88 @@
 import styled, { keyframes } from "styled-components";
-import HeroBG from "../../asset/heroBG1.jpg"
+import HEROBG from "../../asset/HEROBG.jpeg"
 
-const breatheAnimation = keyframes`
- 0% { 
-        background-color: black;
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-    }
+const anim = keyframes`
+    0% { 
+            background-color: black;
+        }
 
- 100% { 
-     background-image: url(${HeroBG});
-             }
+    100% { 
+            background-color: rgba(0,0,0,0.4) ;
+        }
  `
 
-const slideInFromLeft = keyframes`
+const slideInFromBottom = keyframes`
     0% {
-      transform: translateX(100%);
+      transform: translateY(100%);
     }
     100% {
-      transform: translateX(0);
+      transform: translateY(0);
+    }
+`
+
+const FadeIn = keyframes`
+    from {
+        opacity: 0;
+    }
+
+    to {
+        opacity: 1;
     }
 `
 
 export const HomeContainer = styled.div`
-    animation: ${slideInFromLeft} 2s ;
     display: flex;
     width: 100%;
     height: 600px;
-    background-color: #000;
     justify-content: center;
     align-items: center;
     padding-top: 80px;
     flex-direction: column;
-    background-image: url(${HeroBG});
+    animation: ${anim} 2s ease;
+    background-color: rgba(0,0,0,0.4);
+    z-index: -10;
+    /* background-image: url(${HEROBG});
     background-size: cover;
     background-position: center;
-    background-repeat: no-repeat;
+    background-repeat: no-repeat; */
     
     /* &:hover {
         
     } */
 
 `
-
+export const ImageContainer = styled.div`
+    width: 100%;
+    height: 600px;
+    position: absolute;
+    top: 0;
+    z-index: -1;
+    /* animation: ${slideInFromBottom} 3s ease; */
+    background-image: url(${HEROBG});
+    background-position: center;
+    background-size: cover;
+    
+`
 export const Dialogue = styled.h3`
     text-align: center;
     width: 100%;
     max-width: 600px;
     color: #fff;
+    
 `
 
 export const Name = styled.h1`
     font-size: 3.5rem;
     transition: all .5s ease-in-out;
     color: #fff;
+    
     &:hover {
         text-shadow: 2px 2px 6px darkred;
-
     }
-`
-
-export const NameSVG = styled.text`
-    font-family: 'Manrope', sans-serif;
-
-
-
 `
 
 export const Subtitle = styled.h3`
     font-size: 1.5rem;
-    color: #fff;
+    animation: ${FadeIn} 1s ease 1s;
+    color: white;
 `
